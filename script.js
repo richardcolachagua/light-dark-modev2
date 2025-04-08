7358d55adcd023d883b9f09972c9468d22f12c5b
Richard Colachagua <richardcolachagua@gmail.com>
2025-04-07 20:49:19 -0400

    continous updates on light and dark mode toggle

--- script.js
+++ script.js
@@ -6,12 +6,38 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
+// Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.remove("fa-sun");
+  toggleIcon.children[1].classList.remove("fa-moon");
+  image1.src = "img/undraw_proud_coder_dark.svg";
+  image2.src = "img/undraw_feeling_proud_dark.svg";
+  image3.src = "img/undraw_conceptual_idea_dark.svg";
+}
+
+// Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.remove("fa-moon");
+  toggleIcon.children[1].classList.remove("fa-sun");
+  image1.src = "img/undraw_proud_coder_light.svg";
+  image2.src = "img/undraw_feeling_proud_light.svg";
+  image3.src = "img/undraw_conceptual_idea_light.svg";
+}
+
 // Switch theme dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
   }
 }
 
